e45baaa9cab1cd95181b7ac06b513c9883caebb7
xanonity <31986856+xanonity@users.noreply.github.com>
2021-01-23 17:45:32 +0300

    Update Lock.sh

--- Lock.sh
+++ Lock.sh
@@ -3,6 +3,13 @@
 # grep -P 'TTIMEOUT' > result.txt
 #head -n 100 > result.txt
 
+# cat g/rphost*/*.log|
+# perl -pe 's/\xef\xbb\xbf//'|
+# perl -pe 's/^\d+:\d+\.\d+-\d+,Context/,Context/'|
+# perl -n -e 'if (/^\d\d:\d\d\.\d+/) {$event =~ s/\r\n/<line>/g; $event =~ s/\n/<line>/g; print $event."\r\n" if (defined $event); $event = "";} $event .= $_; END{$event =~ s/\r\n/<line>/g; $event =~ s/\n/<line>/g; print $event if (defined $event)};' |
+# grep -P '\d{6,},TLOCK.+WaitConnections=\d+'| 
+# head -n 100 > result.txt
+
 # cat TJ/rphost*/20112018.log|
 # perl -pe 's/\xef\xbb\xbf//; s/\d\d:\d\d\.\d+-\d+,Context/,Context/'|
 # perl -ne 'if (/\d\d:\d\d\.\d+-\d+/) {$s =~ s/\r\n/<NL>/g; $s =~ s/\n/<NL>/g; print "$s\n" if defined $s; $s = ""} $s .= $_; END {$s =~ s/\r\n/<NL>/g; $s =~ s/\n/<NL>/g; print $s if defined $s}'|

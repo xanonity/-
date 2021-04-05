31600064d9c59b9cc422483cae26f1f20ad3ca4d
xanonity <31986856+xanonity@users.noreply.github.com>
2021-04-05 23:07:48 +0300

    Add files via upload

---
+++ Топ событий по длительности.sh
@@ -0,0 +1,7 @@
+cat 1cv8s*/*.log|
+perl -pe 's/\xef\xbb\xbf//g'| 
+grep -P '^\d\d:\d\d\.\d+-'|
+perl -pe 's/^\d\d:\d\d\.\d+-//g'|
+gawk -F',' '{Dur[$2]+=$1; Execs[$2]+=1} END {for (i in Dur) printf "%.3f=TotalDurationSec, %.3f=AvgDurationSec, %d=Execs, %s\n", Dur[i]/1000000, Dur[i]/Execs[i]/1000000, Execs[i], i}'|
+sort -rnb|
+head -n 100 > events_on_duration.txt

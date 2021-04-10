2fdd3477e282b97c48b87ca39e451a90d063f4b6
xanonity <31986856+xanonity@users.noreply.github.com>
2021-04-10 16:31:00 +0300

    Update !Поиск DBMSSQL по части запроса.sh

--- !Поиск DBMSSQL по части запроса.sh
+++ !Поиск DBMSSQL по части запроса.sh
@@ -1,6 +1,6 @@
 cat rphost*/*.log|
 perl -pe 's/\xef\xbb\xbf//'|
-perl -pe 's/^\d\d:\d\d\..+,Context=/,Context=/'|
+perl -pe 's/^\d\d:\d\d\.\d+-\d+,Context/Context/'|
 perl -ne 'if (/^\d\d:\d\d\./) {$s =~ s/\r\n/<NL>/g; $s =~ s/\n/<NL>/g; print "$s\n"; $s = ""} $s .= $_; END {$s =~ s/\r\n/<NL>/g; $s =~ s/\n/<NL>/g; print "$s\n";}'|
 grep -P ',DBMSSQL,.+UPDATE.+_Const300'|
 head -n 100 > DBMSSQL_on_query.txt
